d80bbf0698efea3b03d67773ad2fb6143a56e881
Suntea <xthi.ngu@gmail.com>
2015-09-25 17:48:18 +0200

    Added Match and Round classes
    Organizing the structure of a game

--- src/model/Match.java
+++ src/model/Match.java
@@ -1,18 +1,19 @@
 package model;
 
+import java.util.ArrayList;
+
 /**
- * A match is composed by 4 rounds (seasons)
+ * A match is composed of 4 rounds (seasons)
  * @author Suntea
  *
  */
 public class Match {
-	/**
-	 * Actual season of the match
-	 */
-	private Season season;
+	private ArrayList<Round> rounds = new ArrayList<Round>();
 	
-	public Match() {
-		this.season = Season.SPRING;
+	public void start() {
+		this.rounds.add(new Round(Season.SPRING));
+		
+		this.rounds.get(rounds.size() - 1).execute();
 	}
 
 }
---
+++ src/model/Round.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Round {
+	private Season season;
+	
+	public Round(Season season) {
+		this.season = season;
+	}
+	
+	/**
+	 * Play a round of a match
+	 */
+	public void execute() {
+		
+	}
+
+}
